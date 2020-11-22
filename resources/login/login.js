b9312c2b8dd168b281506c48cb26b3d1c710eb3e
Ramses Almeida <ramsesalmeida@bemol.com.br>
2020-11-22 19:18:46 -0300

    login resources

---
+++ resources/login/login.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "invalid_credencials": {
+    "email": "1234@teste.com.br",
+    "password": "Teste12345!",
+    "assertion_title": "Secure Sign-In | Bitso",
+    "assertion_page": "Your login information is incorrect."
+  }
+}
